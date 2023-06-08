e8c0026b6a2c85d73d394db5bd954806ef67d06d
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-06-08 16:22:30 -0300

    feat: add Ethers provider to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,12 @@ import { Global, Module } from '@nestjs/common';
 import { Client } from 'userop';
 
 import { AccountModule } from './account/account.module';
-import { USER_OPS_CLIENT_TOKEN } from 'src/constants';
+import {
+  ETHERS_PROVIDER_TOKEN,
+  USER_OPS_CLIENT_TOKEN,
+} from 'src/utils/constants';
 import { TransactionModule } from './transaction/transaction.module';
+import { providers } from 'ethers';
 
 @Global()
 @Module({
@@ -22,7 +26,16 @@ import { TransactionModule } from './transaction/transaction.module';
       },
       inject: [ConfigService],
     },
+    {
+      provide: ETHERS_PROVIDER_TOKEN,
+      useFactory: (configService: ConfigService) => {
+        return new providers.JsonRpcProvider(
+          configService.getOrThrow('RPC_URL'),
+        );
+      },
+      inject: [ConfigService],
+    },
   ],
-  exports: [USER_OPS_CLIENT_TOKEN],
+  exports: [USER_OPS_CLIENT_TOKEN, ETHERS_PROVIDER_TOKEN],
 })
 export class AppModule {}
