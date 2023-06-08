585ed669a1d1666020e6d3eed5d6d410b171c921
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2023-06-08 15:09:56 -0300

    feat: add userOps client,ConfigModule and Account & Transaction modules to App

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
-import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Global, Module } from '@nestjs/common';
+import { Client } from 'userop';
 
+import { AccountModule } from './account/account.module';
+import { USER_OPS_CLIENT_TOKEN } from 'src/constants';
+import { TransactionModule } from './transaction/transaction.module';
+
+@Global()
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AccountModule,
+    TransactionModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: USER_OPS_CLIENT_TOKEN,
+      useFactory: (configService: ConfigService) => {
+        return Client.init(configService.getOrThrow('RPC_URL'));
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: [USER_OPS_CLIENT_TOKEN],
 })
 export class AppModule {}
